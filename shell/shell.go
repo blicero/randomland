c05f71c3489d704ff548094a9f697afc23c509f0
Benjamin Walkenhorst <krylon@posteo.de>
2025-03-26 18:52:22 +0100

    Progressing on the Shell, slowly.

--- shell/shell.go
+++ shell/shell.go
@@ -2,19 +2,23 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 25. 03. 2025 by Benjamin Walkenhorst
 // (c) 2025 Benjamin Walkenhorst
-// Time-stamp: <2025-03-25 17:05:40 krylon>
+// Time-stamp: <2025-03-26 18:24:09 krylon>
 
 // Package shell provides the text-based user interface.
 package shell
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/blicero/krylib"
+	"github.com/anmitsu/go-shlex"
 	"github.com/blicero/randomland/common"
 	"github.com/blicero/randomland/logdomain"
 	"github.com/c-bata/go-prompt"
 )
+
+const prefix = ">>> "
 
 var commands = []prompt.Suggest{
 	{Text: "look", Description: "Inspect your surroundings"},
@@ -26,9 +30,9 @@ var commands = []prompt.Suggest{
 	{Text: "quit", Description: "Quit"},
 }
 
-type completer func(d prompt.Document) []prompt.Suggest
+// type completer func(d prompt.Document) []prompt.Suggest
 
-func makeCompleter(sugg []prompt.Suggest) completer {
+func makeCompleter(sugg []prompt.Suggest) func(d prompt.Document) []prompt.Suggest {
 	return func(d prompt.Document) []prompt.Suggest {
 		return prompt.FilterHasPrefix(sugg, d.GetWordBeforeCursor(), true)
 	}
@@ -36,8 +40,10 @@ func makeCompleter(sugg []prompt.Suggest) completer {
 
 // Shell provides a text-based interface to the user.
 type Shell struct {
-	log  *log.Logger
-	cmpl completer
+	log    *log.Logger
+	cmpl   func(d prompt.Document) []prompt.Suggest
+	prompt *prompt.Prompt
+	cmdmap map[string]bool
 }
 
 // New creates a new Shell
@@ -52,11 +58,66 @@ func New() (*Shell, error) {
 	}
 
 	s.cmpl = makeCompleter(commands)
+	s.cmdmap = make(map[string]bool, len(commands))
+
+	for _, c := range commands {
+		s.cmdmap[c.Text] = true
+	}
+
+	s.prompt = prompt.New(
+		s.executor,
+		s.cmpl,
+		prompt.OptionPrefix(prefix),
+	)
 
 	return s, nil
 } // func New() (*Shell, error)
 
 // Run executes the Shell's main loop
 func (s *Shell) Run() error {
-	return krylib.ErrNotImplemented
-}
+	var (
+		err error
+	)
+
+	defer func() {
+		if x := recover(); x != nil {
+			s.log.Printf("[ERROR] Panic while running prompt: %s\n",
+				x)
+		}
+	}()
+
+	s.prompt.Run()
+
+	return err
+} // func (s *Shell) Run() error
+
+func (s *Shell) executor(in string) {
+	s.log.Printf("[TRACE] Attempting to execute %s\n",
+		in)
+
+	var (
+		err    error
+		pieces []string
+		ok     bool
+	)
+
+	if pieces, err = shlex.Split(in, false); err != nil {
+		s.log.Printf("[ERROR] Cannot tokenize input %q: %s\n",
+			in,
+			err.Error())
+	}
+
+	if ok = s.cmdmap[pieces[0]]; !ok {
+		fmt.Printf("Unknown command: %s\n", pieces[0])
+		return
+	}
+
+	switch pieces[0] {
+	case "quit":
+		s.log.Println("[INFO] Bye bye")
+		os.Exit(0)
+	default:
+		s.log.Printf("[ERROR] Command %q is not implemented, yet.\n",
+			pieces[0])
+	}
+} // func (s *Shell) executor(in string)
